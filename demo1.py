1005f1dd02f414c0f66d90f7dfc41d6de038c50e
YXue <xue.326@osu.edu>
2022-04-15 11:59:23 -0400

    add pkg

--- demo1.py
+++ demo1.py
@@ -1,2 +1,13 @@
 import pandas as pd
 import numpy as np
+from numpy.linalg import inv
+import matplotlib.pyplot as plt
+
+'''create objectives, gather info from attributes then search it in data inventory and generate matrix'''
+
+class Process:
+    def __init__(self, name='', county='', state='', country=''):
+        self.name = name
+        self.county = county
+        self.state = state
+        self.country = country
