9ebd81d8a2cf183456c6a97694f40bedbc8e8746
YXue <xue.326@osu.edu>
2022-04-27 18:16:15 -0400

    sharing principle

--- demo1.py
+++ demo1.py
@@ -69,6 +69,29 @@ AA = pd.read_csv('A_3pro.csv')
 DD = pd.read_csv('D_3pro.csv')
 
 
+'''Sharing Principle Data'''
+state_gdp = pd.read_csv('state_gdp.csv')
+state_pop = pd.read_csv('state_pop.csv')
+state_area = pd.read_csv('state_area.csv')
+state_emission = pd.read_csv('state_emission.csv')
+state_invgdp = pd.read_csv('state_invgdp.csv')
+
+county_area = pd.read_csv('county_area.csv')
+county_pop = pd.read_csv('county_pop.csv')
+county_emission = pd.read_csv('county_emission.csv')
+
+region_area = pd.read_csv('region_area.csv', encoding='unicode_escape')
+region_emission = pd.read_csv('region_emission.csv', encoding='unicode_escape')
+region_gdp = pd.read_csv('region_gdp.csv', encoding='unicode_escape')
+region_invgdp = pd.read_csv('region_invgdp.csv', encoding='unicode_escape')
+region_pop = pd.read_csv('region_pop.csv', encoding='unicode_escape')
+
+Global = {'gdp': 8.67e7, 'emission': 4.726e10, 'invgdp': 7.236e-2, 'pop': 7.628e9, 'area': 1.361e8} # unit: million $, ton/yr, 1/million $, km^2
+Nation = {'gdp': region_gdp, 'emission': region_emission, 'invgdp': region_invgdp, 'pop': region_pop, 'area': region_area} #data based on year 2018
+State = {'gdp': state_gdp, 'emission': state_emission, 'invgdp': state_invgdp, 'pop': state_pop, 'area': state_area} #data based on year 2018
+County = {'gdp': '', 'emission': county_emission, 'invgdp': '', 'pop': county_pop, 'area': county_area} #data based on year 2018
+
+
 '''generate A, D, C, m and f martix'''
 Amatrix = np.matrix(AA.values)
 Dmatrix = np.matrix(DD.values)
