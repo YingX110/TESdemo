ef0d4b9d2e5855f92fcad5134d00a535daba4f13
YXue <xue.326@osu.edu>
2022-04-29 19:59:26 -0400

    sharing principle func

--- SharingPrinciple.py
+++ SharingPrinciple.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 state_gdp = pd.read_csv('state_gdp.csv')
 state_pop = pd.read_csv('state_pop.csv')
@@ -21,10 +22,34 @@ Nation = {'gdp': region_gdp, 'emission': region_emission, 'invgdp': region_invgd
 State = {'gdp': state_gdp, 'emission': state_emission, 'invgdp': state_invgdp, 'pop': state_pop, 'area': state_area} #data based on year 2018
 County = {'gdp': '', 'emission': county_emission, 'invgdp': '', 'pop': county_pop, 'area': county_area} #data based on year 2018
 
-def sharing_principle(Region, State, County):
+SPinfo = {'global': Global, 'nation': Nation, 'state': State, 'county': County}
+
+
+def sharing_principle(Nation, State, County, nScales):
     lst = []
-    num_of_scales = input('Number of scales (Local, County, State, Nation, Global): ')
+    #num_of_scales = input('Number of scales (Local, County, State, Nation, Global): ')
     SP = input('Sharing Principle (gdp, inverse of gdp, emission, area, population): ')
-    for i in range(int(num_of_scales)):
+    for i in range(nScales):
         scale = input('Geo-scale' + str(i) + ': ')
+        #lst.append(SPinfo[scale])
         lst.append(scale)
+
+'''get sharing principles between scales
+e.g. list = [global, national]
+principle = 'emission'
+loc = {'global': gname, 'nation': nname, 'state': sname, 'county': cname}
+'''
+def SP_CAL(list, principle, loc):
+    d1 = SPinfo[list[0]][principle] #nation
+    d2 = SPinfo[list[1]][principle] #global
+    s1 = loc[list[0]] #[n1, n2, n3 ...]
+    s2 = loc[list[1]] #[g1, g2, g3 ...]
+    sp_lst = []
+    for i in range(len(s1)):
+        sp = d1.at[0, s1[i]] / d2.at[0, s2[i]]
+        sp_lst.append(sp)
+    return np.diag(sp_lst)
+
+
+
+
