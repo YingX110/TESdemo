5fb23dea9c3a504a639a1ce4b713219c19835378
YingX110 <bourgognexy@hotmail.com>
2022-11-01 00:01:17 -0400

    visual page

--- Home.py
+++ Home.py
@@ -18,5 +18,11 @@ def set_bg_hack(main_bg):
      )
      
 
-set_bg_hack('images/nature.jpg')
+set_bg_hack('images/nature2.jpg')
 
+st.markdown("<h1 style='text-align: center; color: white; font-size: 53px;'>Towards the Global Goal <br> for Nature </h1>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: center; color: white; font-size: 22px;'><q>As humans we are born of the Earth, <br> nourished by the Earth, <br> healed by the Earth</q></p>", unsafe_allow_html=True)
+
+# st.sidebar.write(
+#     f"This app shows how a Streamlit app can interact easily with a to read or store data."
+# )
